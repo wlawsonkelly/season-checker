e33ea523967553465051a76939f8e08e0ccd0cb5
Lawson Kelly <wlawsonkelly@gmail.com>
2020-07-28 14:50:52 -0400

    commit to github

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,36 @@
+import './SeasonDisplay.css';
+import React from 'react';
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+      return  lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter': 'summer';
+    }
+}
+
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach",
+        icnoName: "sun"
+    },
+    winter: {
+        text: "Burr it's chilly",
+        icnoName: "snowflake"
+    }
+};
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const {text, icnoName} = seasonConfig[season];
+
+    return (
+        <div className = {`season-display ${season}`}>
+            <i className = {`icon-left massive ${icnoName} icon`} />
+        <h1>{text}</h1>
+        <i className = {`icon-right massive ${icnoName} icon`} />
+        </div>
+    )
+};
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+
+class App extends React.Component {
+    state = {lat: null};
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({lat: position.coords.latitude});
+            },
+            (err) => this.setState({errorMessage: err.message})
+        );
+    }
+
+    renderContent() {
+        if (this.state.errorMessage && !this.setState.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat}/>;
+        }
+       
+        return (
+        <div>
+            <Spinner message = 'Give Location Please'/>
+        </div>
+        );
+    }
+
+    render() {
+     return(
+         <div className = 'render'>
+            {this.renderContent()}
+         </div>
+     )
+      
+    }
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    <App />,
+    document.querySelector('#root')
+)
